83475a21bb9c40d6e2cd04092c73b7553510c5bb
david222313 <davidavilavalencia7@gmail.com>
2026-05-17 21:38:16 -0500

    chore: añadir clase principal Spring Boot

---
+++ src/main/java/com/picasfijas/app/PicasFijasApplication.java
@@ -0,0 +1,12 @@
+package com.picasfijas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicasFijasApplication {
+
+    public static void main(String[] args) {uhi
+        SpringApplication.run(PicasFijasApplication.class, args);
+    }
+}
